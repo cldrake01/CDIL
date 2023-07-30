01f760f052473531caa6f4d5c202315592fccf34
Collin <collinlindendrake@gmail.com>
2023-07-30 15:09:14 -0600

    //

--- public/animations.js
+++ public/animations.js
@@ -2,6 +2,8 @@ const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('animated-in');
+        } else {
+            entry.target.classList.remove('animated-in');
         }
     });
 });
